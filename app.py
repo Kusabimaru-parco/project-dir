b41655a53858a86529af7873787827d76883aa07
Kusabimaru-parco <juricparcome@gmail.com>
2025-11-06 22:21:01 +0800

    config.py added and followed 2nd video intructions

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+from config import Config
 import os
 
 load_dotenv()
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
