bc96c5b45cf0adf6952b1c06a83781618201c711
Kusabimaru-parco <juricparcome@gmail.com>
2025-11-06 22:34:28 +0800

    models db.py and user_dome.py added, 3rd and 4th video finished

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from config import Config
+from models.db import db
+from models.user_model import User
 import os
 
 load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+db.init_app(app)
 
 @app.route('/')
 def index():
